22fe6a1a8a1660868aa729adfdf3de7b6d36ee52
James Sullivan <james.brian.sullivan@gmail.com>
2023-09-20 15:38:31 -0600

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,26 @@
+import adapter from '@sveltejs/adapter-static';
+import {
+  vitePreprocess
+} from '@sveltejs/kit/vite';
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  extensions: ['.svelte', '.md'],
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/ts-webcrawler-docs' : '',
+        }
+
+
+    prerender: {
+      entries: ['*'],
+      handleMissingId: 'warn',
+    },
+  },
+};
+
+export default config;
